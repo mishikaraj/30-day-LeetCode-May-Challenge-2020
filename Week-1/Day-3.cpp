80cdf5ae4034d55be57654796c6de1f550e084fd
mishikaraj <mishikaraj99@gmail.com>
2020-05-04 12:44:40 +0530

    Ransom-Note

--- Week-1/Day-3.cpp
+++ Week-1/Day-3.cpp
@@ -21,8 +21,7 @@ public:
     for (int i = 0; i < magazine.length(); i++) 
         count[magazine[i]]++; 
           
-    // Now traverse through str2 to check 
-    // if every character has enough counts 
+    
     for (int i = 0; i < ransomNote.length(); i++) 
     { 
         if (count[ransomNote[i]] == 0) 
